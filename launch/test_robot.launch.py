c7b071528dd96d444550bb46bac262acb5d0f90b
NaokiTakahashi <naoki.takahashi060@gmail.com>
2023-03-28 04:18:24 +0900

    lint

--- launch/test_robot.launch.py
+++ launch/test_robot.launch.py
@@ -20,7 +20,9 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-# ros2 topic pub -r 5 /diff_drive_controller/cmd_vel_unstamped geometry_msgs/msg/Twist "{linear: {x: 1.0}, angular: {z: 1.0}}"
+# ros2 topic pub -r 5 \
+# /diff_drive_controller/cmd_vel_unstamped geometry_msgs/msg/Twist \
+# "{linear: {x: 1.0}, angular: {z: 1.0}}"
 
 
 import os
